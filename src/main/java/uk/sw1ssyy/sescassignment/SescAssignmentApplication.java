20e49b22aca8c3c27ea02993138592ca9a7370f2
sw1ssyy <johnanderson8484@gmail.com>
2023-03-02 15:51:21 +0000

    Base Version

---
+++ src/main/java/uk/sw1ssyy/sescassignment/SescAssignmentApplication.java
@@ -0,0 +1,13 @@
+package uk.sw1ssyy.sescassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SescAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SescAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/sw1ssyy/sescassignment/SescAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.sw1ssyy.sescassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SescAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
